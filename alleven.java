4cd4c638af604dad14edad1c5bedfe39e76fbcc7
Nakul Pandey <nkp3128@gmail.com>
2026-06-20 23:22:55 +0530

    Add files via upload

---
+++ alleven.java
@@ -0,0 +1,13 @@
+package LOOP;
+
+public class alleven {
+    public static void main(String args[]){
+        int i=0;
+        while(i<=100){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
