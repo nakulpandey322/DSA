e02b2b19c602b17ef9611eb6e50ff789ef2cc5de
Nakul Pandey <nkp3128@gmail.com>
2026-06-30 22:23:57 +0530

    Add files via upload

---
+++ Areaofrectangle.java
@@ -0,0 +1,10 @@
+package METHODS;
+
+public class Areaofrectangle {
+    public static int Area(int length,int width){
+        return length*width;
+    }
+    public static void main(String args[]){
+        System.out.println(Area(5,4));
+    }
+}
