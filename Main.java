a5aa1a747dbb7668d199fb652f5eb5ab429192b7
Nakul Pandey <nkp3128@gmail.com>
2026-07-17 21:13:59 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Student {
+    // Private data members
+    private String name;
+    private int age;
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.setName("John");
+        s.setAge(20);
+
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
