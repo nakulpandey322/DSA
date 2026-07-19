7fad33185595ab5d132818e96d202b394d593418
Nakul Pandey <nkp3128@gmail.com>
2026-07-19 23:03:15 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,21 @@
+package ARRAY.inputoutput;
+
+public class reverse {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length / 2; i++) {
+
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
