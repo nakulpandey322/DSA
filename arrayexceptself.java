006d697111d5aee5a1da30262da92e2911b7bfde
Nakul Pandey <nkp3128@gmail.com>
2026-08-14 23:36:08 +0530

    Add files via upload

---
+++ arrayexceptself.java
@@ -0,0 +1,40 @@
+package ARRAY.inputoutput;
+import java.util.Arrays;
+public class arrayexceptself {
+        public static int[] productExceptSelf(int[] nums) {
+
+            int n = nums.length;
+
+            int[] answer = new int[n];
+
+            // Prefix Product
+            answer[0] = 1;
+
+            for (int i = 1; i < n; i++) {
+
+                answer[i] = answer[i - 1] * nums[i - 1];
+            }
+
+            // Suffix Product
+            int suffix = 1;
+
+            for (int i = n - 1; i >= 0; i--) {
+
+                answer[i] = answer[i] * suffix;
+
+                suffix *= nums[i];
+            }
+
+            return answer;
+        }
+
+        public static void main(String[] args) {
+
+            int[] nums = {1, 2, 3, 4};
+
+            int[] result = productExceptSelf(nums);
+
+            System.out.println(Arrays.toString(result));
+        }
+    }
+
