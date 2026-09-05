af39eb38c682cb03a270600f44e8e750086ae4ef
Nakul Pandey <nkp3128@gmail.com>
2026-09-05 22:01:38 +0530

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class BFS {
+    static void bfs(int[][] graph, int start, int n) {
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int i = 0; i < n; i++) {
+                if (graph[node][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 1, 1, 0, 0},
+            {1, 0, 0, 1, 0},
+            {1, 0, 0, 0, 1},
+            {0, 1, 0, 0, 1},
+            {0, 0, 1, 1, 0}
+        };
+
+        bfs(graph, 0, 5);
+    }
+}
