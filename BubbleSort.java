14bf8be7f60eddfb90acc044536250bbaaf09d29
Nakul Pandey <nkp3128@gmail.com>
2026-09-12 22:03:12 +0530

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,29 +1,23 @@
-import java.util.Scanner;
-
+package ARRAY.inputoutput;
+import java.util.Arrays;
 public class BubbleSort {
-    public static void main(String[] args) {
-        int n,temp;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of elements: ");
-        n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        for (int i=0;i<n-1;i++) {
-            for (int j=0; j<n-1-i;j++) {
-                if (arr[j]>arr[j+1]){
-                    temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+    static void bubbleSort(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
                 }
             }
-        }
+    }
 
-        System.out.println("Sorted array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+    public static void main(String args[]) {
+        int[] arr = {5, 6, 8, 7, 2, 5, 4, 3, 1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
